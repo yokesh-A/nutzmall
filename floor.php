83ded433f4259fa37be0a1a4bf8a82e203472d98
yokesh-a <bossyokesh3377@gmail.com>
2020-08-11 20:31:40 +0530

    skin

--- floor.php
+++ floor.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Interactive 3D Mall Map | Codrops</title>
+		<title>Block A</title>
 		<meta name="description" content="An interactive 3D mall map concept with a search and pin indicators for every level. " />
 		<meta name="keywords" content="mall map, 3d, css, javascript, pin, levels, floor map" />
 		<meta name="author" content="Codrops" />
